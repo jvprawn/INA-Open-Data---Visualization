29e367e89c2924e65497bdf7dfa004ec5b317186
Julivius Prawira <5561924+jvprawn@users.noreply.github.com>
2020-05-16 18:37:41 +0200

    First update

--- Main visualization.R
+++ Main visualization.R
@@ -1,8 +1,13 @@
-#import needed package
-
-#read file inside folder
-getwd()
-setwd( system.getCurrentDirectory())
-
+#import package
 library(here)
-install.packages("here")
+library(ggplot2)
+
+#read file into R
+df1 <- read.csv(here("data","daftar-importir-2019-.csv"))
+summary(df1) #it is literally a list of importer in the city
+
+df2 <- read.csv(here("data","data-siup-kota-batu-2017-.csv"),header=FALSE)
+summary(df2) #another hard to digest data
+
+df3 <- read.csv(here("data","jumlah-pohon-menghasilkan-dan-produksi-buah-2016.csv"),header=FALSE)
+summary(df3) #much better
